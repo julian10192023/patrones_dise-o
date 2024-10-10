790014886e9011353014b3676e44229117d90ffd
julian10192023 <142471561+julian10192023@users.noreply.github.com>
2024-10-09 20:02:50 -0500

    creacion de proyecto

--- co.edu.uniquindi.patrones_disenio/src/main/java/co/edu/uniquindi/patrones_disenio/Main.java
+++ co.edu.uniquindi.patrones_disenio/src/main/java/co/edu/uniquindi/patrones_disenio/Main.java
@@ -1,4 +1,4 @@
-package co.edu.uniquindio.PatronAbstractFactory;
+package co.edu.uniquindi.patrones_disenio;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
